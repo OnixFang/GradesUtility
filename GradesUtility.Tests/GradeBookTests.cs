c8a272dab5af0cbae7da37c295b655d160b7832d
OnixFang <twily.dev@outlook.com>
2018-05-25 00:36:08 -0400

    Update unit tests

--- GradesUtility.Tests/GradeBookTests.cs
+++ GradesUtility.Tests/GradeBookTests.cs
@@ -11,6 +11,16 @@ namespace GradesUtility.Tests
     [TestClass]
     public class GradeBookTests
     {
+        static void WriteResults(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result}.");
+        }
+
+        static void WriteResults(string description, string result)
+        {
+            Console.WriteLine($"{description}: {result}.");
+        }
+
         [TestMethod]
         public void ComputeHighestGrade()
         {
@@ -20,6 +30,9 @@ namespace GradesUtility.Tests
             book.AddGrade(89);
 
             GradeStatistics stats = book.ComputeStatistics();
+
+            WriteResults("Highest Grade", stats.HighestGrade);
+            WriteResults("Expected value", 90);
 
             Assert.AreEqual(90, stats.HighestGrade);
         }
@@ -33,6 +46,9 @@ namespace GradesUtility.Tests
             book.AddGrade(89);
 
             GradeStatistics stats = book.ComputeStatistics();
+
+            WriteResults("Lowest Grade", stats.LowestGrade);
+            WriteResults("Expected value", 89);
 
             Assert.AreEqual(89, stats.LowestGrade);
         }
@@ -48,7 +64,28 @@ namespace GradesUtility.Tests
 
             GradeStatistics stats = book.ComputeStatistics();
 
+            WriteResults("Average Grade", stats.AverageGrade);
+            WriteResults("Approximate expected value", 85.66f);
+
             Assert.AreEqual(85.66f, stats.AverageGrade, 0.01);
+        }
+
+        [TestMethod]
+        public void ComputeLetterGrade()
+        {
+            GradeBook book = new GradeBook();
+
+            book.AddGrade(80);
+            book.AddGrade(85.4f);
+            book.AddGrade(71);
+            book.AddGrade(69);
+
+            GradeStatistics stats = book.ComputeStatistics();
+
+            Console.WriteLine($"Average Grade: {stats.AverageGrade}, Letter Grade: {stats.LetterGrade}.");
+            WriteResults("Expected value", "C");
+
+            Assert.AreEqual("C", stats.LetterGrade);
         }
     }
 }
